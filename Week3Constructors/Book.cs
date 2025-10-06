1be6e5f68a456377873eaa94858eb63733d5d7fa
Finlay Morgan <Morgan-F5@ulster.ac.uk>
2025-10-06 16:10:10 +0100

    22

--- Week3Constructors/Book.cs
+++ Week3Constructors/Book.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Week3Constructors
+namespace Library
 {
     class Book
     {
+        string Title;
+        string Author;
+        string ISBN;
+
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
             Title = bookTitle;
@@ -15,21 +19,12 @@ namespace Week3Constructors
             ISBN = bookISBN;
         }
 
-        void DisplayInfo()
+        public void DisplayInfo()
         {
             Console.WriteLine($"Book title: {Title}");
             Console.WriteLine($"Book Author: {Author}");
             Console.WriteLine($"Book ISBN: {ISBN}");
             Console.WriteLine();
-        }
-
-        static void Main(string[] args)
-        {
-            // Create a new instance (object) of the Book class
-            // Note how the object name differs from the class name
-            Book book = new Book("C# for beginners", "Bill Gates", "1234567");
-
-            book.DisplayInfo();
         }
     }
 }
