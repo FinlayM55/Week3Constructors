adb41389a10f204aed814c1ca9e730adaf260239
Finlay Morgan <Morgan-F5@ulster.ac.uk>
2025-10-06 16:49:04 +0100

    44

---
+++ Week3Constructors/Member.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Constructors
+{
+    internal class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+        public int MemberId
+        { 
+            get { return memberId; } 
+            set { memberId = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.MemberId = memberId;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"MemberId: {MemberId}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Phone: {Phone}");
+            Console.WriteLine();
+        }
+    }
+}
--- Week3Constructors/Program.cs
+++ Week3Constructors/Program.cs
@@ -1,14 +1,25 @@
 ﻿using Library;
+using Week3Constructors;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Create a new instance (object) of the Book class
-        // Note how the object name differs from the class name
         Book book = new Book("C# for beginners", "Bill Gates", "1234567"); 
         Book book1 = new Book("Ultimate C#", "Microsoft", "2233445");
+        Book book2 = new Book("C# 2 Electric Boogaloo", "Microsoft", "2233446");
+        Console.WriteLine("Current available books");
         book.DisplayInfo();
+        book1.DisplayInfo();
+        book2.DisplayInfo();
+
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+        Member member2 = new Member(3, "Muhammad Lee", "64 Zoo Lane", 0793786290);
+        Console.WriteLine("Current library members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
+        member2.DisplayInfo();
     }
 }
 
