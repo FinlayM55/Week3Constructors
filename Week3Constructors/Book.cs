cb35ae20262f7613f21c8ec86c0c7ffbd393deb8
Finlay Morgan <Morgan-F5@ulster.ac.uk>
2025-10-06 15:55:18 +0100

    Add project files.

---
+++ Week3Constructors/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Constructors
+{
+    class Book
+    {
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+
+        void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            // Create a new instance (object) of the Book class
+            // Note how the object name differs from the class name
+            Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+
+            book.DisplayInfo();
+        }
+    }
+}
